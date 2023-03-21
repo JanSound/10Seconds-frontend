fe1ebe22b55545e46d529eab03a1057105105f68
ghenmaru <rbgks1937@naver.com>
2023-03-22 01:31:35 +0900

    Feat: 음성 녹음 기능 구현

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
@@ -8,27 +8,61 @@ import {
   Button,
 } from 'react-native';
 
+import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+
 StatusBar.setBarStyle('light-content');
 
 import { PERMISSIONS, RESULTS, request } from 'react-native-permissions';
 
+const audioRecorderPlayer = new AudioRecorderPlayer();
 const App = () => {
-  const checkRecord = async () => {
+  const [recording, setRecording] = useState(false);
+  const [mikePermission, setMikePermission] = useState(false);
+  const [recordDuration, setRecordDuration] = useState({
+    recordSecs: 0,
+    recordTime: '',
+  });
+
+  const handleStartRecord = async () => {
+    if (audioRecorderPlayer) {
+      setRecording(true);
+      await audioRecorderPlayer.startRecorder();
+    }
+    audioRecorderPlayer.addRecordBackListener((e) => {
+      setRecordDuration({
+        ...recordDuration,
+        recordSecs: e.currentPosition,
+        recordTime: audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)),
+      });
+    });
+  };
+
+  const handleStopRecord = async () => {
+    if (audioRecorderPlayer) {
+      setRecording(false);
+      await audioRecorderPlayer.stopRecorder();
+    }
+    audioRecorderPlayer.removeRecordBackListener();
+    setRecordDuration({ ...recordDuration, recordSecs: 0 });
+  };
+
+  const checkRecordPermission = async () => {
     try {
-      const result = await request(PERMISSIONS.IOS.SPEECH_RECOGNITION);
-      if (result === RESULTS.GRANTED) {
-        console.log('성공');
-      }
+      await request(PERMISSIONS.IOS.SPEECH_RECOGNITION).then((result) => {
+        if (result === 'granted') {
+          setMikePermission(true);
+          console.log('성공');
+        }
+      });
     } catch (e) {
       console.log(`에러 \n ${e}`);
     }
   };
 
-  const handleRecord = () => {};
-
   useEffect(() => {
-    checkRecord();
-  }, []);
+    checkRecordPermission();
+  }, [mikePermission]);
+
   return (
     <View style={styles.container}>
       <SafeAreaView style={{ backgroundColor: 'black' }}></SafeAreaView>
@@ -36,7 +70,19 @@ const App = () => {
         <Text style={styles.title}>10Seconds</Text>
       </View>
       <View style={styles.body}>
-        <Button title="음성 녹음" color="black" onPress={handleRecord}></Button>
+        {recording ? (
+          <Button
+            title="Stop"
+            color="black"
+            onPress={handleStopRecord}
+          ></Button>
+        ) : (
+          <Button
+            title="Recording"
+            color="black"
+            onPress={handleStartRecord}
+          ></Button>
+        )}
       </View>
       <View style={styles.footerNavigation}>
         <Button title="tab1"></Button>
