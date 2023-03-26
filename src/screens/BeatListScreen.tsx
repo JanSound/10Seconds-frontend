f33aafe0a1a448db55db1afddb554d8d038576a5
rbgksqkr <rbgks1937@naver.com>
2023-03-26 16:38:59 +0900

    Design: BeatList 화면 스타일링

--- src/screens/BeatListScreen.tsx
+++ src/screens/BeatListScreen.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import EditHeader from '../common/header/EditHeader';
 import Footer from '../Footer';
 
 const BeatListScreen = ({ navigation }: any) => {
+  const [beats, setBeats] = useState([
+    { name: 'beat 1' },
+    { name: 'beat 2' },
+    { name: 'beat 3' },
+    { name: 'beat 4' },
+    { name: 'beat 5' },
+    { name: 'beat 6' },
+    { name: 'beat 7' },
+  ]);
   return (
     <>
       <EditHeader navigation={navigation} />
       <View style={styles.beatListContainer}>
-        <Image source={require('../assets/images/drum.png')} />
+        <View style={styles.beatList}>
+          {beats.map((beat: { name: string }) => (
+            <View style={styles.beat}>
+              <Text>{beat.name}</Text>
+            </View>
+          ))}
+        </View>
       </View>
-
       <Footer navigation={navigation} />
     </>
   );
@@ -20,6 +34,23 @@ const styles = StyleSheet.create({
   beatListContainer: {
     flex: 1,
     backgroundColor: 'gray',
+  },
+  beatList: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  beat: {
+    width: 300,
+    height: 50,
+    margin: 10,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    borderColor: 'black',
+    borderWidth: 3,
+    justifyContent: 'center',
+  },
+  beatContent: {
+    fontSize: 20,
   },
 });
 
