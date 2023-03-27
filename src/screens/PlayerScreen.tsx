b5510e68eb2e9c94d260bb4dbe9c9c025bec2588
rbgksqkr <rbgks1937@naver.com>
2023-03-27 20:37:54 +0900

    Feat: BeatList에서 Player로 넘어갈 때 비트에 맞는 이미지 불러오기

--- src/screens/PlayerScreen.tsx
+++ src/screens/PlayerScreen.tsx
@@ -1,22 +1,50 @@
 import Header from '@/common/header/Header';
 import Footer from '../Footer';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageSourcePropType,
+  Image,
+} from 'react-native';
+import PlayBtn from '@/common/button/PlayBtn';
+
+interface IInstrument {
+  [key: string]: ImageSourcePropType;
+}
+
+const instrument: IInstrument = {
+  base: require('../assets/images/base.png'),
+  piano: require('../assets/images/piano.png'),
+  drum: require('../assets/images/drum.png'),
+};
 
 const PlayerScreen = (props: any) => {
   const { route, navigation } = props;
   return (
-    <View style={styles.playerContainer}>
+    <>
       <Header navigation={navigation} />
-      <Text>{route.params.instType}</Text>
+      <View style={styles.playerContainer}>
+        <Image
+          style={styles.instrumentImage}
+          source={instrument[route.params.instType]}
+        />
+      </View>
       <Footer />
-    </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   playerContainer: {
     flex: 1,
+    backgroundColor: 'gray',
+  },
+  instrumentImage: {
+    width: 100,
+    height: 100,
+    margin: 10,
   },
 });
 export default PlayerScreen;
