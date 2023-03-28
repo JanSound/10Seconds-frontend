5b77dbb84d54c5927b56b00d91739d4daae170a9
rbgksqkr <rbgks1937@naver.com>
2023-03-29 02:28:31 +0900

    Feat: SelectScreen과 PlayerScreen 라우팅

--- src/screens/SelectScreen.tsx
+++ src/screens/SelectScreen.tsx
@@ -1,28 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
 import Footer from '../Footer';
 import Header from '../common/header/Header';
 
+interface IInstrument {
+  [key: string]: string;
+}
+const instrumentId: IInstrument = {
+  base: 'same beat base url',
+  piano: 'same beat paino url',
+  drum: 'same beat drum url',
+};
+
 const SelectScreen = ({ navigation }: any) => {
+  const [beats, setBeats] = useState([
+    {
+      id: 'same beat other instType url',
+      instType: 'base',
+    },
+    {
+      id: 'same beat other instType url2',
+      instType: 'piano',
+    },
+    {
+      id: 'same beat other instType url3',
+      instType: 'drum',
+    },
+  ]);
+  // input: 악기 선택(instType 선택)
+  const playBeat = (instType: string) => {
+    navigation.navigate('Player', {
+      beatId: instrumentId[instType],
+      instType: instType,
+    });
+  };
+
   return (
     <>
       <Header />
       <View style={styles.instrumentContainer}>
-        <TouchableOpacity style={styles.baseContainer}>
+        <TouchableOpacity
+          style={styles.baseContainer}
+          onPress={() => playBeat('base')}
+        >
           <Image
             style={styles.base}
             source={require('../assets/images/base.png')}
           />
           <Text style={{ fontWeight: 'bold', fontSize: 30 }}>base</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.pianoContainer}>
+        <TouchableOpacity
+          style={styles.pianoContainer}
+          onPress={() => playBeat('piano')}
+        >
           <Image
             style={styles.piano}
             source={require('../assets/images/piano.png')}
           />
           <Text style={{ fontWeight: 'bold', fontSize: 30 }}>piano</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.drumContainer}>
+        <TouchableOpacity
+          style={styles.drumContainer}
+          onPress={() => playBeat('drum')}
+        >
           <Image
             style={styles.drum}
             source={require('../assets/images/drum.png')}
