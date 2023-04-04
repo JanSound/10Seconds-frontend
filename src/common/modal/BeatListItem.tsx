328c22fbc4059848981b165afa921aa9e8e10dae
rbgksqkr <rbgks1937@naver.com>
2023-04-04 14:38:49 +0900

    Refactor: inline 스타일링 StyleSheet로 분리

--- src/common/modal/BeatListItem.tsx
+++ src/common/modal/BeatListItem.tsx
@@ -4,22 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 const BeatListItem = (props: any) => {
   const { beat } = props;
   return (
-    <View
-      style={{
-        height: 60,
-        paddingLeft: 20,
-        justifyContent: 'center',
-        borderBottomColor: 'black',
-        borderBottomWidth: StyleSheet.hairlineWidth,
-      }}
-      key={beat.id}
-    >
-      <Text style={{ fontSize: 20, fontFamily: 'NotoSansKR-Bold' }}>
-        {beat.name}
-      </Text>
-      <Text style={{ fontSize: 15 }}>{beat.instType}</Text>
+    <View style={styles.beatContainer}>
+      <Text style={styles.beatName}>{beat.name}</Text>
+      <Text style={styles.beatInstType}>{beat.instType}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  beatContainer: {
+    height: 60,
+    paddingLeft: 20,
+    justifyContent: 'center',
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  beatName: { fontSize: 20, fontFamily: 'NotoSansKR-Bold' },
+  beatInstType: {
+    fontSize: 15,
+  },
+});
 
 export default BeatListItem;
