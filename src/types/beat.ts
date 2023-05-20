cf2ecb7f85262d803508221290d0e069db245413
rbgksqkr <rbgks1937@naver.com>
2023-05-20 21:57:13 +0900

    Refactor: interface 분리

---
+++ src/types/beat.ts
@@ -0,0 +1,16 @@
+export interface IBeat {
+  id: string;
+  name: string;
+  beatType: string;
+  presignedUrl: string;
+  createdAt: string;
+  checked: boolean;
+  clicked: boolean;
+}
+
+export interface IFetchBeat {
+  ID: string;
+  BeatType: string;
+  PresignedUrl: string;
+  RegTs: string;
+}
